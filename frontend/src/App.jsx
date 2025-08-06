bc455deb23962f104ba1c9ab9620e9e6256b0882
Sre-Sac <susac.srecko@rolla.app>
2025-08-06 18:38:18 +0200

    feat: day 1 setup - frontend + backend + vite + express

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold underline text-red-600">
+        Hello FitVenture!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
