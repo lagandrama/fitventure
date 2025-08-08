d5d1bca8aba31dea9444ea7d38c3bc87ad7beb7e
lagandrama <susacsrecko@gmail.com>
2025-08-08 02:00:38 +0200

    comm

--- backend/index.js
+++ backend/index.js
@@ -17,5 +17,9 @@ app.get('/api/test', (req, res) => {
   res.json({ message: 'Backend radi!' });
 });
 
+app.get('/', (req, res) => {
+  res.send('FitVenture API is running!');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
